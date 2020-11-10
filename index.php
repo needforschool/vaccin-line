524e38430234da5c022ead6e079c95592209c233
VitorinoNzo <vitorinonzo@gmail.com>
2020-11-10 11:46:24 +0100

    css

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="asset/css/style.css">
+  <link rel="stylesheet" href="asset/css/style-front.css">
 </head>
 <body>
   <div class="wrap">
