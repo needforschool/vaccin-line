e059af852527af53148573dcde7827c650f0a1b8
VitorinoNzo <vitorinonzo@gmail.com>
2020-11-10 14:56:53 +0100

    footer/css

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="asset/css/style-front.css">
-</head>
-<body>
-  <div class="wrap">
-    <header>
-      <div class="wrap-header">
-        <div class="logo-header">
-          <a href="#"><div class="logo"></div></a>
-        </div>
-        <div class="nav">
-          <nav>
-            <ul class="navbar">
-              <li><a href="#">home</a></li>
-              <li><a href="#">inscription</a></li>
-              <li><a href="#">connexion</a></li>
-              <li><a href="#">déconnexion</a></li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </header>
-    <div class="container">
 
 
 
@@ -32,34 +5,4 @@
 
 
 
-    </div><!-- .container -->
-    <footer class="footer">
-        <div wrap="wrap-footer">
-            <div class="columns">
-                <div class="column">
-                    <h3 class="column-title">Contactez-nous</h3>
-                    <img src="#" alt="Vaccin'Line"><br><br> 
-                    <ul class="social">
-                        <li><a href="#" class="facebook"></a></li>
-                        <li><a href="#" class="instagram"></a></li>
-                        <li><a href="#" class="twitter"></a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="logo-footer">
-                 <a href="#"><div class="logo"></div></a>
-            </div>
-            <div class="bottom-bar">
-                <p cass="copyright"> &copy;Copyright | Vaccin'Corp 2020 | all rights reserved.</p>
-                <nav>
-                  <ul>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Mentions légales</a></li>
-                  </ul>
-                </nav>
-            </div>
-        </div>
-    </footer>
-  </div><!-- .wrap -->
-</body>
-</html>
+
