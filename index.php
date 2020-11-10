7ffe20ffde4313f6d4291923beabefffa1cb4b88
QVan27 <qvannarath@gmail.com>
2020-11-10 13:39:57 +0100

    ajout du header

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="asset/css/style-front.css">
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 </head>
 <body>
   <div class="wrap">
@@ -19,6 +20,7 @@
               <li><a href="#">inscription</a></li>
               <li><a href="#">connexion</a></li>
               <li><a href="#">déconnexion</a></li>
+              <li><a href="admin/index.php">admin</a></li>
             </ul>
           </nav>
         </div>
