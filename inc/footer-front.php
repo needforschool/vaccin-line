c4ce20d9f884c7f9e53e29baa234b6ad77e6e526
QVan27 <qvannarath@gmail.com>
2020-11-10 16:53:16 +0100

    page accueil

--- inc/footer-front.php
+++ inc/footer-front.php
@@ -1,30 +1,30 @@
 </div><!-- .container -->
-  <footer class="footer">
-      <div wrap="wrap-footer">
-          <div class="columns">
-              <div class="column">
-                  <h3 class="column-title">Contactez-nous</h3>
-                  <img src="#" alt="Vaccin'Line"><br><br>
-                  <ul class="social">
-                      <li><a href="#" class="facebook"></a></li>
-                      <li><a href="#" class="instagram"></a></li>
-                      <li><a href="#" class="twitter"></a></li>
-                  </ul>
-              </div>
-          </div>
-          <div class="logo-footer">
-               <a href="#"><div class="logo"></div></a>
-          </div>
-          <div class="bottom-bar">
-              <p cass="copyright"> &copy;Copyright | Vaccin'Corp 2020 | all rights reserved.</p>
-              <nav>
-                <ul>
-                  <li><a href="#">Contact</a></li>
-                  <li><a href="#">Mentions légales</a></li>
-                </ul>
-              </nav>
-          </div>
+<footer class="footer">
+  <div wrap="wrap-footer">
+    <div class="columns">
+      <div class="column">
+        <h3 class="column-title">Contactez-nous</h3>
+        <img src="#" alt="Vaccin'Line"><br><br>
+        <ul class="social">
+          <li><a href="#" class="facebook"></a></li>
+          <li><a href="#" class="instagram"></a></li>
+          <li><a href="#" class="twitter"></a></li>
+        </ul>
       </div>
-  </footer>
+    </div>
+    <div class="logo-footer">
+      <a href="#"><div class="logo"></div></a>
+    </div>
+    <div class="bottom-bar">
+      <p cass="copyright"> &copy;Copyright | Vaccin'Corp 2020 | all rights reserved.</p>
+      <nav>
+        <ul>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Mentions légales</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</footer>
 </body>
 </html>
