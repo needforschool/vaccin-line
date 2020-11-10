bde55b2426a4fa1f85f2d99225cbe15664cd0382
QVan27 <qvannarath@gmail.com>
2020-11-10 11:01:22 +0100

    structure html

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-//Index utilisateur
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body>
+  <div class="wrap">
+    <header>
+      <nav>
+        <ul>
+          <li><a href="index.php"></a></li>
+          <li><a href="inscription.php"></a></li>
+          <li><a href="connexion.php"></a></li>
+          <li><a href="logout.php"></a></li>
+        </ul>
+      </nav>
+    </header>
+    <div class="container">
+
+
+
+
+
+
+
+    </div><!-- .container -->
+    <footer>
+
+    </footer>
+  </div><!-- .wrap -->
+</body>
+</html>
