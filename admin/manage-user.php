9216fa64a35a6337580cc8f0906571e00bc4e563
ImJustLucas <contact@lucasbellier.fr>
2020-11-11 13:50:47 +0100

    Modif Autorisation

--- admin/manage-user.php
+++ admin/manage-user.php
@@ -3,11 +3,9 @@ include('../inc/pdo.php');
 include('../inc/function.php');
 if (!est_connecte()) {
   header('Location: 403.php');
-} elseif ($_SESSION['user']['role'] != 'role_admin') {
+  } elseif ($_SESSION['user']['role'] != 'role_admin') {
   header('Location: 403.php');
-} else {
-  $admin = true;
-}
+  }
 $title = 'Manage user';
 
 include('inc/header-back.php');
