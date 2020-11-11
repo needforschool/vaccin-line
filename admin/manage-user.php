706d29dd4feb16b59ef5214acf6cc9ebf0414a61
ImJustLucas <contact@lucasbellier.fr>
2020-11-11 14:56:35 +0100

    Ajout gestion user

--- admin/manage-user.php
+++ admin/manage-user.php
@@ -9,6 +9,11 @@ if (!est_connecte()) {
   }
 $title = 'Manage user';
 
+$sql = "SELECT * FROM vl_users WHERE status = 1";
+$query = $pdo->prepare($sql);
+$query->execute();
+$users = $query->fetchAll();
+
 include('inc/header-back.php');
  ?>
 
@@ -18,6 +23,35 @@ include('inc/header-back.php');
 
         <!-- Page Heading -->
         <h1 class="h3 mb-4 text-gray-800"><?php echo $title; ?></h1>
+
+        <div class="d-flex flex-wrap justify-content-between">
+
+          <?php foreach ($users as $user) :?>
+            <div class="col-xl-4 col-md-6 mb-4">
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary"><?php echo mb_strtoupper($user['nom']) . ' ' . ucfirst($user['prenom']); ?></h6>
+                </div>
+                <div class="card-body">
+                  <p>Date de naissance : <?php echo ucfirst($user['date_naissance']); ?></p>
+                  <p>Age : <?php echo ucfirst($user['age']); ?></p>
+                  <p><?php echo ucfirst($user['email']); ?></p>
+                  <p>Civilité : <?php echo ucfirst($user['civilite']); ?></p>
+                  <a href="#" class="btn btn-info btn-circle">
+                      <i class="fas fa-info-circle"></i>
+                  </a>
+                  <a href="#" class="btn btn-danger btn-circle">
+                      <i class="fas fa-trash"></i>
+                  </a>
+                </div>
+              </div>
+            </div>
+          <?php endforeach ;?>
+
+
+
+        </div>
+
 
     </div>
     <!-- /.container-fluid -->
