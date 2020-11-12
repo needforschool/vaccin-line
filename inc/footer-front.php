fe94dc1563cde158ed1593125201e1cf52089e65
VitorinoNzo <vitorinonzo@gmail.com>
2020-11-12 10:29:46 +0100

    footer

--- inc/footer-front.php
+++ inc/footer-front.php
@@ -4,24 +4,16 @@
   <div class="wrap-footer">
     <div class="columns">
       <div class="column1">
-        <h3 class="column-title">Contactez-nous :</h3>
-        <img src="asset/img/header-logo.png" alt="Vaccin'Line">
-        <a href="#">main road, building name, country</a>
-        <a href="#">info@company.com</a>
+      <img src="asset/img/header-logo.png" alt="Vaccin'Line">
       </div>
       <div class="column2">
         <a href="#"><i class="fab fa-facebook-square"></i></a>
         <a href="#"><i class="fab fa-twitter-square"></i></a>
       </div>
       <div class="column3">
-        <h3 class="column-title">Created by :</h3>
-        <ul class="creator">
-          <li><a href="#" class="lucas"></a><img src="asset/img/avatar-lucas.png" alt="Lucas" title="ImJustLucas"></li>
-          <li><a href="#" class="enzo"></a><img src="asset/img/avatar-enzo.png" alt="Enzo" title="Vitorino Nzo"></li>
-          <li><a href="#" class="florian"></a><img src="asset/img/avatar-florian.png" alt="Florian" title="Legilmalas"></li>
-          <li><a href="#" class="quentin"></a><img src="asset/img/avatar-quentin.png" alt="Quentin" title="Natty"></li>
-          <li><a href="#" class="valentin"></a><img src="asset/img/avatar-valentin.png" alt="Valentin"title="Dzr"></li>
-        </ul>
+        <h3 class="column-title">Contactez-nous :</h3>
+        <a href="#">main road, building name, country</a>
+        <a href="#">info@company.com</a>
       </div>
     </div>
   </div>
