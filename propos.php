f86d1c12fac7b6a813cc54f106a584895b0e9a08
QVan27 <qvannarath@gmail.com>
2020-11-13 16:38:20 +0100

    about modif

--- propos.php
+++ propos.php
@@ -24,40 +24,45 @@ include('inc/header-front.php');?>
       </div>
     </div>
     <div class="bigbox-start">
-      <div class="stats">
-        <h1 id="lucas">Lucas AKA ImJustLucas</h1><p class="role">(Chef de projet et Team Back)</p>
+      <div class="stats-about">
+        <h1 id="lucas">Lucas AKA ImJustLucas<span class="age"> (19)</span></h1><p class="role">(Chef de projet et Team Back)</p>
+        <p>Le chef / la chef de projet web gère l'ensemble d'un projet de site internet, qu'il s'agisse d'une création ou d'une refonte. Il analyse dans le détail les besoins du client et rédige le cahier des charges, puis coordonne le travail de tous les acteurs du projet.</p>
       </div>
       <div class="img-stats">
         <img src="asset/img/lucas.jpg" alt="">
       </div>
     </div>
     <div class="bigbox4">
-      <div class="stats">
-        <h1 id="enzo">Enzo AKA VitorinoNzo</h1><p class="role">(Team Front)</p>
+      <div class="stats-about">
+        <h1 id="enzo">Enzo AKA VitorinoNzo<span class="age"> (19)</span></h1><p class="role">(Team Front)</p>
+        <p>Un développeur front-end est un professionnel de l'informatique et/ou du web design capable de produire des sites web en utilisant ses connaissances en HTML, CSS et javascript, éventuellement couplées à des compétences dans le domaines d'un ou de plusieurs gestionnaires de contenus (CMS).</p>
       </div>
       <div class="img-stats">
         <img src="asset/img/enzo.png" alt="">
       </div>
     </div>
     <div class="bigbox3">
-      <div class="stats">
-        <h1 id="florian">Florian AKA Legilmalas</h1><p class="role">(Team Front)</p>
+      <div class="stats-about">
+        <h1 id="florian">Florian AKA Legilmalas<span class="age"> (19)</span></h1><p class="role">(Team Front)</p>
+        <p>Un développeur front-end est un professionnel de l'informatique et/ou du web design capable de produire des sites web en utilisant ses connaissances en HTML, CSS et javascript, éventuellement couplées à des compétences dans le domaines d'un ou de plusieurs gestionnaires de contenus (CMS).</p>
       </div>
       <div class="img-stats">
         <img src="asset/img/florian.png" alt="">
       </div>
     </div>
     <div class="bigbox4">
-      <div class="stats">
-        <h1 id="quentin">Quentin AKA Natty</h1><p class="role">(Team Front)</p>
+      <div class="stats-about">
+        <h1 id="quentin">Quentin AKA Natty<span class="age"> (27)</span></h1><p class="role">(Team Front)</p>
+        <p>Un développeur front-end est un professionnel de l'informatique et/ou du web design capable de produire des sites web en utilisant ses connaissances en HTML, CSS et javascript, éventuellement couplées à des compétences dans le domaines d'un ou de plusieurs gestionnaires de contenus (CMS).</p>
       </div>
       <div class="img-stats">
         <img src="asset/img/quentin.jpg" alt="">
       </div>
     </div>
     <div class="bigbox-end">
-      <div class="stats">
-        <h1 id="valentin">Valentin AKA Dzr</h1><p class="role">(Team Back)</p>
+      <div class="stats-about">
+        <h1 id="valentin">Valentin AKA Dzr<span class="age"> (19)</span></h1><p class="role">(Team Back)</p>
+        <p>Le développeur back-end gère toute la partie « non visible » du développement : il conçoit les éléments techniques nécessaires au fonctionnement d'un site, il se charge de la gestion des bases de données… Il a donc une connaissance approfondie des langages de programmation tels que PHP, Ruby, Python, Java, .</p>
       </div>
       <div class="img-stats">
         <img src="asset/img/valentin.jpg" alt="">
