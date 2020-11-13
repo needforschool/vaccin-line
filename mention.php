c52a1212d4f926b20147b4bab7971f95293819d7
QVan27 <qvannarath@gmail.com>
2020-11-13 17:12:31 +0100

    mentions end

--- mention.php
+++ mention.php
@@ -13,6 +13,7 @@ include('inc/header-front.php');?>
 <section id="mention">
   <div class="wrap-section-mention">
     <div class="mentions-legales">
+      <div class="fond-mention">
       <h2>Mentions légales</h2>
       <p>Conformément aux dispositions de la loi n° 2004-575 du 21 juin 2004 pour la confiance en l’économie numérique, il est précisé aux utilisateurs du site vaccin'line.fr l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi.</p>
       <h2>Edition du site</h2>
@@ -27,6 +28,7 @@ include('inc/header-front.php');?>
       <p>La société Vaccin'Corp conservera dans ses systèmes informatiques et dans des conditions raisonnables de sécurité une preuve de la transaction comprenant le bon de commande et la facture.</p>
       <p>La société Vaccin'Corp a fait l’objet d’une déclaration à la CNIL sous le numéro 1986932.</p>
       <p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’Utilisateur dispose d’un droit d’accès, de modification et de suppression des informations collectées par Vaccin'Corp. Pour exercer ce droit, il reviendra à l’Utilisateur d’envoyer un message à l’adresse suivante : vaccin-line@gmail.com</p>
+      </div>
     </div>
   </div>
 </section>
