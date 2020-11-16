b271f0ac955a041f48fe484b9ececa37d6fa904e
ImJustLucas <contact@lucasbellier.fr>
2020-11-16 15:27:07 +0100

    Now we can see the role of the user

--- admin/single-user.php
+++ admin/single-user.php
@@ -136,14 +136,32 @@ include('inc/header-back.php');
         </div>
 
         <div class="row justify-content-center">
-          <!-- Suppression d'un utilisateur -->
-          <a href="#" data-href="#" data-toggle="modal" data-target="#confirm-delete"  class="btn btn-danger btn-icon-split">
-            <span class="icon text-white-50">
-              <i class="fas fa-trash"></i>
-            </span>
-            <span class="text">Supprimer utilisateur</span>
-          </a>
 
+          <div class="col-xl-4 col-md-6 mb-4">
+              <div class="card border-left-<?php if($user['role'] == 'role_admin'){ echo 'danger'; } else {echo 'primary';} ?> shadow h-100 py-2">
+                  <div class="card-body">
+                      <div class="row no-gutters align-items-center">
+                          <div class="col mr-2">
+                              <div class="text-xs font-weight-bold text-<?php if($user['role'] == 'role_admin'){ echo 'danger'; } else {echo 'primary';} ?> text-uppercase mb-1">
+                                  Role</div>
+                              <div class="h5 mb-0 font-weight-bold text-gray-800"><?php if($user['role'] == 'role_admin'){ echo 'Administrateur'; } else {echo 'Utilisateur';} ?></div>
+                          </div>
+                          <div class="col-auto">
+                              <i class="fas fa-<?php if($user['role'] == 'role_admin'){ echo 'user-cog'; } else {echo 'user';} ?> fa-2x text-gray-300"></i>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="mt-4">
+            <!-- Suppression d'un utilisateur -->
+            <a href="#" data-href="#" data-toggle="modal" data-target="#confirm-delete"  class="btn btn-danger btn-icon-split">
+              <span class="icon text-white-50">
+                <i class="fas fa-trash"></i>
+              </span>
+              <span class="text">Supprimer utilisateur</span>
+            </a>
+          </div>
         </div>
 
 
