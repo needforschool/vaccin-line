809874def1734deb705177bbd6e8ac3d666d25a2
ImJustLucas <contact@lucasbellier.fr>
2020-11-16 10:17:17 +0100

    Ajout class strong

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if(!empty($_POST['ajoutvaccin'])) {
     <div class="bigbox">
       <div class="stats">
         <h1>Comment utiliser notre carnet ?</h1>
-        <p>Le carnet de vaccination est un carnet dans lequel sont notées toutes les vaccinations d’une personne. Ce carnet est très <strong href="very-important-word">pratique</strong> : il vous permet de savoir quelles vaccinations vous avez reçues et si vous êtes à jour. Il vous suffit donc de vous connecter et ne pas oublier de le présenter au professionnel de santé à chaque fois que vous vous faites vacciner. Il est valable toute la vie ! Pour l’obtenir, il suffit de vous <a href="signin.php"><strong>inscrire</strong></a>. Il est <strong href="very-important-word">gratuit</strong>.</p>
+        <p>Le carnet de vaccination est un carnet dans lequel sont notées toutes les vaccinations d’une personne. Ce carnet est très <strong class="very-important-word">pratique</strong> : il vous permet de savoir quelles vaccinations vous avez reçues et si vous êtes à jour. Il vous suffit donc de vous connecter et ne pas oublier de le présenter au professionnel de santé à chaque fois que vous vous faites vacciner. Il est valable toute la vie ! Pour l’obtenir, il suffit de vous <a class="signin.php"><strong>inscrire</strong></a>. Il est <strong class="very-important-word">gratuit</strong>.</p>
       </div>
       <!-- <div class="img-stats">
         <img src="asset/img/medical.jpg" alt="">
@@ -53,7 +53,7 @@ if(!empty($_POST['ajoutvaccin'])) {
     <div class="bigbox2">
       <div class="stats2">
         <h1>Notre carnet est-il sécurisé ?</h1>
-        <p>Vos données medicale sont <strong href="very-important-word">précieuses</strong>, et doivent être protégées. C'est pour cela que nous avons mis au point un puissant logiciel de cryptage afin de vous proposer une <strong href="very-important-word">sécurité maximale</strong>. Enfin, vos données sont stockées dans une base de données spéciale, reconnue par le <strong href="very-important-word">gouvernement</strong>. Avec Vaccin 'line, vos données sont protégées et ne seront jamais partagées.</p>
+        <p>Vos données medicale sont <strong class="very-important-word">précieuses</strong>, et doivent être protégées. C'est pour cela que nous avons mis au point un puissant logiciel de cryptage afin de vous proposer une <strong class="very-important-word">sécurité maximale</strong>. Enfin, vos données sont stockées dans une base de données spéciale, reconnue par le <strong class="very-important-word">gouvernement</strong>. Avec Vaccin 'line, vos données sont protégées et ne seront jamais partagées.</p>
       </div>
       <!-- <div class="img-stats">
         <img src="asset/img/img-box2.jpg" alt="">
