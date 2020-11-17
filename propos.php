d435a8d75341ff20cdaa74cf6a32e657ca8227de
QVan27 <qvannarath@gmail.com>
2020-11-17 12:04:30 +0100

    about

--- propos.php
+++ propos.php
@@ -11,20 +11,51 @@ $errors = array();
 include('inc/header-front.php');?>
 <script type="text/javascript" src="asset/js/konami.js"></script>
 <section id="about">
-  <div class="wrap-about">
-    <div class="bigbox">
-      <div class="stats">
-        <h1>Qui sommes-nous ?</h1>
-        <p>Nous ? Nous sommes juste des étudiants en première année d'un bachelor en développement web, rien de plus ! Entre la team front composée de <a href="#quentin"><strong>Quentin Vannarath</strong></a>, <a href="#enzo"><strong>Enzo Vitorino</strong></a> et <a href="#florian"><strong>Florian Galvani</strong></a>, la team back composée de <a href="#lucas"><strong>Lucas Bellier</strong></a> et <a href="#valentin"><strong>Valentin Lamy</strong></a>, la concurence a du soucis a se faire !</p>
+<div class="title-text">
+      <p>À propos</p>
+      <h1>Notre histoire</h1>
+    </div>
+    <div class="about-box">
+      <div class="abouts">
+        <h1>Qui sommes nous</h1>
+        <div class="abouts-desc">
+          <div class="about-icon">
+            <i class="fa fa-user-circle"></i>
+          </div>
+          <div class="about-text"></div>
+          <p>Nous sommes juste des étudiants en première année d'un bachelor en développement web, rien de plus ! Entre la team front composée de <a href="#quentin"><strong>Quentin Vannarath</strong></a>, <a href="#enzo"><strong>Enzo Vitorino</strong></a> et <a href="#florian"><strong>Florian Galvani</strong></a>, la team back composée de <a href="#lucas"><strong>Lucas Bellier</strong></a> et <a href="#valentin"><strong>Valentin Lamy</strong></a>, la concurence a du soucis a se faire !</p>
+        </div>
+        <h1>Projet: Carnet de Vaccination</h1>
+        <div class="abouts-desc">
+          <div class="about-icon">
+            <i class="fa fa-history"></i>
+          </div>
+          <div class="about-text"></div>
+          <p>Vaccin Line est un projet que nous avons dû réaliser en deux semaines (du 09/11/2020 au 20/11/2020), avec pour demande de développer une application web permettant aux clients de renseigner leur carnet de vaccination afin d'automatiser celui-ci (informer sur les différents vaccins, rappeler la date des rappels), tout en protégeant les données de nos utilisateurs.</p>
+        </div>
+        <h1>Konami Code#</h1>
+        <div class="abouts-desc">
+          <div class="about-icon-hint">
+            <i class="fa fa-arrow-up"></i>
+            <i class="fa fa-arrow-up"></i>
+            <i class="fa fa-arrow-down"></i>
+            <i class="fa fa-arrow-down"></i>
+            <i class="fa fa-arrow-left"></i>
+            <i class="fa fa-arrow-right "></i>
+            <i class="fa fa-arrow-left"></i>
+            <i class="fa fa-arrow-right "></i>
+            <i class="fa fa-bold"></i>
+            <i class="fa fa-amazon"></i>
+          </div>
+        </div>
+      </div>
+      <div class="abouts-img">
+        <img src="asset/img/feature.jpg" alt="">
       </div>
     </div>
-    <div class="bigbox2">
-      <div class="stats">
-        <h1>L'histoire de Vaccin'Line</h1>
-        <p>Vaccin'Line est un projet que nous avons dû réaliser en 2 semaines (du 09/11/2020 au 20/11/2020), avec pour demande de développer une application web permettant aux clients de renseigner leur carnet de vaccination afin d'automatiser celui-ci (informer sur les différents vaccins, rappeler la date des rappels), tout en protégeant les données de nos utilisateurs.</p>
-      </div>
-    </div>
-    <div id="konamcode">
+    
+    <div class="wrap-about">
+    <div id="konamicode">
       <div class="bigbox-start">
         <div class="stats-about">
           <h1 id="lucas">Lucas AKA ImJustLucas<span class="age"> (19)</span></h1><p class="role">(Chef de projet et Team Back)</p>
